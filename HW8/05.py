1ea866bee0bd00ed25e9a4ae0524b3f0bc37a798
Deven Abisha 林順義 <s111310521@student.nqu.edu.tw>
2025-04-26 12:11:11 +0800

    Create 05.py

---
+++ HW8/05.py
@@ -0,0 +1,13 @@
+def grade(n):
+    if 90 <= n <= 100:
+        return 'A'
+    elif 80 <= n < 90:
+        return 'B'
+    elif 70 <= n < 80:
+        return 'C'
+    elif 60 <= n < 70:
+        return 'D'
+    else :
+        return 'F'
+    
+print(grade(89.9))
